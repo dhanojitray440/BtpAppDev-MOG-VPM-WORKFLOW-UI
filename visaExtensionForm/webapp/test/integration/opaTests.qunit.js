f039d44d79c5596ab4d52b80a22930a6b4f951b3
Dhanjit Ray <dhanojit.ray@inkitsolutions.com>
2025-11-21 11:05:47 +0000

    Initial commit

---
+++ visaExtensionForm/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["visaextension/visaExtensionForm/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ visaExtensionForm/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"visaextension/visaExtensionForm/test/unit/controller/MyTaskUI.controller"
+], function () {
+	"use strict";
+});
